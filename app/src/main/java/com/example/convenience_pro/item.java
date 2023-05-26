e3ec81c85af290e505c566736810667bb3820f63
dahye <tizlspffl@naver.com>
2023-05-26 16:59:50 +0900

    품목 리스트 화면 추가

---
+++ app/src/main/java/com/example/convenience_pro/item.java
@@ -0,0 +1,8 @@
+package com.example.convenience_pro;
+
+public class item {
+    public int id;
+    public String itemName;
+    public String itemPrice;
+    public String itemImg;
+}
---
+++ app/src/main/java/com/example/convenience_pro/itmeList.java
@@ -0,0 +1,11 @@
+package com.example.convenience_pro;
+
+import java.util.ArrayList;
+
+public class itmeList {
+    public int id;
+    public String itemListName;
+    public String itemListPrice;
+    public ArrayList<item> items;
+
+}
